f9443a3c24f48f6eb1efaed76221cb78d681c3e0
shubhamkumarsinha25 <shubhamkumarsinha25@gmail.com>
2021-03-14 14:00:18 +0530

    Upload

---
+++ First_Repeating_Element.cpp
@@ -0,0 +1,17 @@
+int firstRepeated(int arr[], int n) {
+    //code here
+    unordered_map<int,int>m;
+    for(int i=0;i<n;i++)
+    {
+        m[arr[i]]++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(m[arr[i]]>1)
+        {
+            return i+1;
+        }
+    }
+    return -1;
+  
+}
